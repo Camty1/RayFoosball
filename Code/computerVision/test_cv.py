7380f080d35346b5a77441ca182e543e6c97fd3d
jacklange22 <skierjack22@gmail.com>
2023-04-22 14:13:20 -0400

    adding cv testing

--- Code/computerVision/test_cv.py
+++ Code/computerVision/test_cv.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+
+#cv
+
+from picamera import PiCamera
+from time import sleep
+#import opencv as cv
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(5)
+camera.stop_preview()
+
+camera.capture('Desktop/test.jpg')
