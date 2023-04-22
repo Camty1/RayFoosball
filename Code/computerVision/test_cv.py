0c0971fef962ab85df143a02f0be0d8844e0b1d4
jacklange22 <skierjack22@gmail.com>
2023-04-22 15:31:24 -0400

    loop with images

--- Code/computerVision/test_cv.py
+++ Code/computerVision/test_cv.py
@@ -3,12 +3,25 @@
 
 from picamera import PiCamera
 from time import sleep
-#import opencv as cv
+import cv2
+import numpy as np
+import os
+
+path = './computerVision/images/'
+for name in os.listdir(path):
+    os.remove(os.path.join(path,name))
 
 camera = PiCamera()
+camera.resolution = (1280,720)
+camera.framerate = 60
 
 camera.start_preview()
-sleep(5)
+#sleep(5)
 camera.stop_preview()
 
-camera.capture('Desktop/test.jpg')
+#camera.capture('../../Desktop/test2.png')
+
+for i in range(10):
+    fileName = path + "image{}.jpg".format(i)
+    camera.capture(fileName)
+    
