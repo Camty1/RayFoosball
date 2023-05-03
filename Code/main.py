0a7e883c13a690f0186715b23be418fcdbc3bf37
jacklange22 <skierjack22@gmail.com>
2023-05-02 20:14:18 -0400

    a couple edits to the cv main testing files

--- Code/main.py
+++ Code/main.py
@@ -2,13 +2,17 @@
 #designed to take everything and integrate into one file
 #no clue how to do this but my functions are in here
 
-from computerVision.cv import ball
-from computerVision.cv import setCamera
+from computerVision.cvCopy import ball
+from computerVision.cvCopy import setCamera
+import time
 
 #setCamera()
-ball = ball('ball.png')
-position = ball.findLocation(0,0)
-
+ball = ball('./Code/ball.png')
+tic = time.perf_counter()
+for i in range(10):
+    position = ball.findLocation(0,0)
+toc = time.perf_counter()
+print({toc-tic})
 
 def runGame():
     position = ball.findLocation(position)
